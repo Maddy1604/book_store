62380f4122310fb888e826cb0c840214e882d670
Maddy1604 <mahehslavadne16400@gmail.com>
2024-10-26 13:56:11 +0530

    [Mahesh] Add: Refactor the GET User API

--- user_services/route.py
+++ user_services/route.py
@@ -247,7 +247,7 @@ def auth_user(request: Request, token: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error while verifiying because invalid token.")
 
 # GET api for takes user ids and validate that ids and return the response
-@app.get('/users', status_code=200, include_in_schema=True)
+@app.get('/users', status_code=200, include_in_schema=False)
 @limiter.limit("100/minute") 
 def get_users(request: Request, user_ids : List[int] = Query([]), db : Session = Depends(get_db)):
     """
